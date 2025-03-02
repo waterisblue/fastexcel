e55dc9e7f8c9abcb2e2048a85ffcddd42884ad91
tison <wander4096@gmail.com>
2025-03-02 14:44:31 +0800

    chore: drop old code (#244)

--- src/test/java/plus/easyexcel/test/demo/read/ReadTest.java
+++
@@ -1,3 +0,0 @@
-/**
- * Documents have been migrated to <a href="https://github.com/CodePhiliaX/fastexcel/blob/master/easyexcel-test/src/test/java/com/CodePhiliaX/fastexcel/test/demo/read/ReadTest.java">https://github.com/CodePhiliaX/fastexcel/blob/master/easyexcel-test/src/test/java/com/CodePhiliaX/fastexcel/test/demo/read/ReadTest.java</a>
- */
--- src/test/java/plus/easyexcel/test/demo/web/WebTest.java
+++
@@ -1,3 +0,0 @@
-/**
- * Documents have been migrated to <a href="https://github.com/CodePhiliaX/fastexcel/blob/master/easyexcel-test/src/test/java/com/CodePhiliaX/fastexcel/test/demo/web/WebTest.java">https://github.com/CodePhiliaX/fastexcel/blob/master/easyexcel-test/src/test/java/com/CodePhiliaX/fastexcel/test/demo/web/WebTest.java</a>
- */
